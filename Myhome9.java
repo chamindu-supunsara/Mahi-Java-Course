c3ad1298701f7d029884c0e4ddc01f12bcb32983
chamisupu <chami2170@gmail.com>
2022-11-01 20:51:28 +0530

    Find Maximum Simple

---
+++ Myhome9.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Myhome9{
+	public static void main(String args[]){
+		int number1 =  25;
+		int number2 =  78;
+		int number3 =  87;
+		
+		int max_num = number1;
+		if (max_num < number2){
+			max_num = number2;
+			if (max_num < number3){
+				max_num = number3;
+					if (max_num < number3){
+						max_num = number3;
+				}
+		}
+		System.out.println("The Greatest: " + max_num);
+		}
+	}
+}
+	
+	
+	
+
+
+
