29eb3ac6c913a21617310790442ec4ceed97f4d7
chamisupu <chami2170@gmail.com>
2022-11-01 20:10:27 +0530

    Random Number With Range

---
+++ Myhome7.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Myhome7{
+	public static void main(String args[]){
+		//Scanner input = new Scanner(System.in);
+		//System.out.println("Enter Number - ");
+		Random input = new Random();
+		int number =  input.nextInt(20);
+		System.out.println(number);		
+	}
+}
+	
+	
+	
+
+
+
