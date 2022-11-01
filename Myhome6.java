2f1cd94751c4c50edee9ecf6694e1bed5f83a8c1
chamisupu <chami2170@gmail.com>
2022-11-01 20:07:13 +0530

    Random Number

---
+++ Myhome6.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Myhome6{
+	public static void main(String args[]){
+		//Scanner input = new Scanner(System.in);
+		//System.out.println("Enter Number - ");
+		Random input = new Random();
+		int number =  input.nextInt();
+		System.out.println(number);		
+	}
+}
+	
+	
+	
+
+
+
