05a60cdaf87e944c3f67c1f6140d65128bf5a560
chamisupu <chami2170@gmail.com>
2022-11-02 10:44:43 +0530

    Switch Case With Break

---
+++ Mywork3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Mywork3{
+	public static void main(String args[]){
+		Scanner input = new Scanner (System.in);
+		int mark =  input.nextInt();
+		switch (mark){
+			case 1 : System.out.println("A");break;
+			case 2 : System.out.println("B");break;
+			case 3 : System.out.println("C");break;
+			case 4 : System.out.println("D");break;
+			case 5 : System.out.println("E");break;
+			default : System.out.println("End Switch");
+		}
+	}
+}
+	
+	
+	
+
+
+
