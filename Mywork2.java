da068e70ee53e2c81012bf64734b9710c30ec687
chamisupu <chami2170@gmail.com>
2022-11-02 10:34:43 +0530

    Switch Case

---
+++ Mywork2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Mywork2{
+	public static void main(String args[]){
+		Scanner input = new Scanner (System.in);
+		int mark =  input.nextInt();
+		switch (mark){
+			case 1 : System.out.println("A");
+			case 2 : System.out.println("B");
+			case 3 : System.out.println("C");
+			case 4 : System.out.println("D");
+			case 5 : System.out.println("E");
+			default : System.out.println("End Switch");
+		}
+	}
+}
+	
+	
+	
+
+
+
