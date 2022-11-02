32dc44955faf9fcab2e4e0f9b1007ed160fe1482
chamisupu <chami2170@gmail.com>
2022-11-02 11:58:08 +0530

    Final variable Keyword

---
+++ Mywork5.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Mywork5{
+	public static void main(String args[]){
+		final int x = 10;
+		int y = 20;
+		//x = 50; Final variable cant change 
+		y = 40;
+		System.out.println(x);
+		System.out.println(y);	
+	}
+}
+	
+	
+	
+
+
+
