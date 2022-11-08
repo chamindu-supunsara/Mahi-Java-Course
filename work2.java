936acb1b0ea1924d4f9ef3845de03a85aee5ab93
chamisupu <chami2170@gmail.com>
2022-11-08 11:53:24 +0530

    For loop 2

---
+++ work2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class work2{
+	public static void main(String args[]){
+		Scanner input = new Scanner (System.in);
+		System.out.println("Number - ");
+		int num =  input.nextInt();
+
+		for (int i = 0;(i <= 100); i += num){
+			System.out.println(i);
+		}
+	}
+}
+	
+	
+	
+
+
+
