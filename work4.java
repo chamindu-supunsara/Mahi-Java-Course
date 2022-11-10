e6134f646af25bdee81eb63d263104e119d8c471
chamisupu <chami2170@gmail.com>
2022-11-10 12:14:03 +0530

    for loop 4

---
+++ work4.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class work4{
+	public static void main(String args[]){
+		for (int i=1; (i <=20); i++){
+			for (int j = 1; (j <= i); j++){
+				System.out.print("*");
+			}
+		System.out.print(" ");
+		}
+	}
+}
+	
+	
+	
+
+
+
