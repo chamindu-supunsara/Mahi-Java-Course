219e0a623f10a9cb7ef7bd3a89d30219271a1411
chamisupu <chami2170@gmail.com>
2022-11-12 12:10:48 +0530

    Break Key Word

---
+++ work10.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class work10{
+	public static void main(String args[]){
+		int i = 1;
+		while(true){
+			if(i == 50)break;
+			System.out.println(i);
+			i++;
+		}
+	}
+}	
+
+
+	
+
+
+
