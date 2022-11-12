1b499bb8a18c2279aa5ac530bb893c3dc517153c
chamisupu <chami2170@gmail.com>
2022-11-12 12:18:56 +0530

    Continue Keyword

---
+++ work11.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class work11{
+	public static void main(String args[]){
+		int i = 1;
+		while((++i) <= 20){
+			if(i%5 == 0)continue;
+			System.out.println(i);
+		}
+	}
+}	
+
+
+	
+
+
+
