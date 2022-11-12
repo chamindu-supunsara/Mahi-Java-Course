82b388a857512a41f5d0bf9eab28468e3d5c4391
chamisupu <chami2170@gmail.com>
2022-11-12 11:28:40 +0530

    Do-While vs While Loop

---
+++ work9.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class work9{
+	public static void main(String args[]){
+		int i = 0;
+		System.out.println("Start Do-While");
+		do{
+			System.out.println(i);
+		}while ((i++)<= 10);
+		System.out.println("End Do-While");
+		
+		i = 0;
+		
+		System.out.println("Start While");
+		while((++i)<=10){
+			System.out.println(i);
+		}
+		System.out.println("End While");
+	}
+}	
+
+
+	
+
+
+
